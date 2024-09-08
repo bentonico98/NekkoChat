e24831712f9b62c9f1f7bf798d485b14b7ad275d
LennyMGarcia <Lennymgr27@gmail.com>
2024-09-08 17:08:34 -0400

    Feat: Se agrego Grupos

--- NekkoChat.Server/Controllers/GroupsControllers/GroupController.cs
+++ NekkoChat.Server/Controllers/GroupsControllers/GroupController.cs
@@ -8,24 +8,24 @@ namespace NekkoChat.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class UserController : ControllerBase
+    public class GroupController : ControllerBase
     {
-        private readonly ILogger<UserController> _logger;
+        private readonly ILogger<GroupController> _logger;
 
-        public UserController(ILogger<UserController> logger)
+        public GroupController(ILogger<GroupController> logger)
         {
             _logger = logger;
         }
         //Busca toda la DATA del usuario basada en el id, el id aqui se busca en base a 2-1, 2-2, 2-3
         //asi se dividio ElasticSearch, esta esta pensada para busquedas mas especificas basadas en el _doc deseado
         [HttpGet("data/{id}")]
-        public async Task<IActionResult> GetAllUSerDataById([FromRoute] string id)
+        public async Task<IActionResult> GetAllGroupDataById([FromRoute] string id)
         {
             try
             {
                 var client = new ElasticsearchClient();
 
-                var response = await client.GetAsync<ElasticUserDTO>(id, idx => idx.Index("nekko_chat_beta_users"));
+                var response = await client.GetAsync<ElasticGroupDTO>(id, idx => idx.Index("nekko_chat_beta_groups"));
 
                 if (!response.IsValidResponse)
                 {
@@ -43,24 +43,24 @@ namespace NekkoChat.Server.Controllers
         //Busca la conversacion en un periodo de tiempo, esta fue pensada para obtener la conversacion semanal o en el periodo que se quiera
         //y no sobrecargar la DB con pedidos, se pide en baches 
         [HttpGet("conversation/{id}/{startDate}/{endDate}")]
-        public async Task<IActionResult> GetConversationByRangeOfDateById([FromRoute] string id, [FromRoute] DateTime startDate, [FromRoute] DateTime endDate)
+        public async Task<IActionResult> GetGroupConversationByRangeOfDateById([FromRoute] string id, [FromRoute] DateTime startDate, [FromRoute] DateTime endDate)
         {
             try
             {
                 var client = new ElasticsearchClient();            
 
-                var response = await client.SearchAsync<ElasticUserDTO>(s => s
-                    .Index("nekko_chat_beta_users")
+                var response = await client.SearchAsync<ElasticGroupDTO>(s => s
+                    .Index("nekko_chat_beta_groups")
                     .Query(q => q
                         .Bool(b => b
                             .Must(f => f
                                 .Term(t => t
-                                    .Field("user_days_json.result.data.conversation_id")
+                                    .Field("group_days_json.result.data.group_id")
                                     .Value(id))
                             ).Filter(f2 => f2 
                                 .Range(r => r
                                     .DateRange(dr => dr
-                                        .Field("user_days_json.result.data.messages.created_at")
+                                        .Field("group_days_json.result.data.messages.created_at")
                                         .Gte(startDate)
                                         .Lte(endDate)
                                         
@@ -83,21 +83,21 @@ namespace NekkoChat.Server.Controllers
         }
         //Busca todas las conversaciones basadas en ese asi, el ordern es descendete por lo que muestra la mas reciente primero
         [HttpGet("conversation/all/{id}")]
-        public async Task<IActionResult> GetAllConversationById([FromRoute] string id)
+        public async Task<IActionResult> GetAllGroupConversationById([FromRoute] string id)
         {
             try
             {
                 var client = new ElasticsearchClient();            
 
-                var response = await client.SearchAsync<ElasticUserDTO>(s => s
-                    .Index("nekko_chat_beta_users")
+                var response = await client.SearchAsync<ElasticGroupDTO>(s => s
+                    .Index("nekko_chat_beta_groups")
                     .Query(q => q
                         .Match(m => m
-                            .Field("user_days_json.result.data.conversation_id")
+                            .Field("group_days_json.result.data.group_id")
                             .Query(id)
                     )
                     ).Sort(s => s
-                        .Field("user_days_json.result.date")
+                        .Field("group_days_json.result.date")
                         .Doc(d => d
                             .Order(SortOrder.Desc)
                         )
@@ -119,21 +119,21 @@ namespace NekkoChat.Server.Controllers
         }
         //Busca la connversacion mas reciente basada en el dia (busca obtener el dia completo)
         [HttpGet("conversation/{id}")]
-        public async Task<IActionResult> GetRecentDayConversationById([FromRoute] string id)
+        public async Task<IActionResult> GetRecentDayGroupConversationById([FromRoute] string id)
         {
             try
             {
                 var client = new ElasticsearchClient();            
 
-                var response = await client.SearchAsync<ElasticUserDTO>(s => s
-                    .Index("nekko_chat_beta_users")
+                var response = await client.SearchAsync<ElasticGroupDTO>(s => s
+                    .Index("nekko_chat_beta_groups")
                     .Query(q => q
                         .Match(m => m
-                            .Field("user_days_json.result.data.conversation_id")
+                            .Field("group_days_json.result.data.group_id")
                             .Query(id)
                     )
                     ).Sort(s => s
-                        .Field("user_days_json.result.date")
+                        .Field("group_days_json.result.date")
                         .Doc(d => d
                             .Order(SortOrder.Desc)
                         )

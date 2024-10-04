afc37f8a7be74b9cc2a0e532d1a05b604f3860fb
lennyMGarcia <lennymgr27@gmail.com>
2024-10-04 11:58:49 -0400

    Feat: solucion del problema de conexion

--- nekkochat.client/src/Pages/VideoCall/VideoCall.tsx
+++ nekkochat.client/src/Pages/VideoCall/VideoCall.tsx
@@ -5,7 +5,7 @@ import VideocamIcon from '@mui/icons-material/Videocam';
 import VideocamOffIcon from '@mui/icons-material/VideocamOff';
 import MicIcon from '@mui/icons-material/Mic';
 import MicOffIcon from '@mui/icons-material/MicOff';
-import { useParams } from 'react-router-dom';
+//import { useParams } from 'react-router-dom';
 import { HubConnectionBuilder } from '@microsoft/signalr';
 
 export const VideoCall: React.FC = () => {
@@ -17,10 +17,10 @@ export const VideoCall: React.FC = () => {
 
     const [isVideoOn, setIsVideoOn] = useState<boolean>(false);
     const [isMicOn, setIsMicOn] = useState<boolean>(false);
-    const [offer2, setIsOffer2] = useState<RTCSessionDescriptionInit | undefined>(undefined);
+    // [offer2, setIsOffer2] = useState<RTCSessionDescriptionInit | undefined>(undefined);
 
-    const { id } = useParams();
-    const [isOffer, setIsOffer] = useState(false);
+    //const { id } = useParams();
+    //const [isOffer, setIsOffer] = useState(false);
 
     const connection = new HubConnectionBuilder()
         .withUrl("https://localhost:7198/privatechathub", { withCredentials: false })
@@ -29,30 +29,12 @@ export const VideoCall: React.FC = () => {
 
     connection.start().catch((err) => console.error(err));
 
-    connection.on('offer', (sdp) => {
-        try {
-            setIsOffer2(() => ({ type: 'offer', sdp }));
-            if (peerConnection.current) {
-                peerConnection.current.setRemoteDescription({ type: 'offer', sdp });
-                peerConnection.current.createAnswer((answer: RTCSessionDescriptionInit) => {
-                    if (answer && answer.sdp) {
-                        peerConnection.current?.setLocalDescription({ type: 'answer', sdp: answer.sdp });
-                        console.log('Enviando answer:', answer);
-                    } else {
-                        console.error('La descripción de sesión SDP no es válida');
-                    }
-                });
-            }
-        } catch (error) {
-            console.error('Error al actualizar la descripción de sesión SDP:', error);
-        }
-    });
+    let answer: RTCSessionDescriptionInit;
 
-    connection.on('answer', (sdp) => {
+    connection.on('offer', async (sdp) => {
         try {
-            setIsOffer2(() => ({ type: 'answer', sdp }));
             if (peerConnection.current) {
-                peerConnection.current.setRemoteDescription({ type: 'answer', sdp });
+                answer = JSON.parse(sdp); 
             }
         } catch (error) {
             console.error('Error al actualizar la descripción de sesión SDP:', error);
@@ -66,6 +48,8 @@ export const VideoCall: React.FC = () => {
         }
     });
 
+
+
     connection.on("connectionStarted", () => {
         console.log('Conexión establecida');
         if (peerConnection.current) {
@@ -77,14 +61,6 @@ export const VideoCall: React.FC = () => {
             };
         }  
     });
-
-    useEffect(() => {
-        if (id === '2') {
-            setIsOffer(false);
-        } else {
-            setIsOffer(true);
-        }
-    }, [id]);
 
     useEffect(() => {
         peerConnection.current = new RTCPeerConnection()
@@ -132,6 +108,15 @@ export const VideoCall: React.FC = () => {
                 connection.invoke('SendIceCandidate', event.candidate.candidate).catch((err) => console.error(err));
             }
         };
+      
+        peerConnection.current.oniceconnectionstatechange = (event) => {
+            console.log('Cambio de estado de la conexión ICE: ' + event, peerConnection.current?.iceConnectionState);
+        };
+
+       
+        peerConnection.current.onconnectionstatechange = (event) => {
+            console.log('Cambio de estado de la conexión: ' + event, peerConnection.current?.connectionState);
+        };
 
         peerConnection.current.ontrack = event => {
             console.log('Se estableció la conexión');
@@ -156,7 +141,16 @@ export const VideoCall: React.FC = () => {
             }
         };
 
-    }, [isVideoOn, isMicOn, isOffer, connection]);
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+            .then(stream => {
+                localStream.current = stream;
+                stream.getTracks().forEach(track => {
+                    peerConnection.current?.addTrack(track, stream);
+                });
+            })
+            .catch(error => console.error('Error obteniendo los datos del video:', error));
+
+    }, [isVideoOn, isMicOn, connection]);
 
     const handleVideoState = () => {
         setIsVideoOn(() => !isVideoOn);
@@ -175,18 +169,25 @@ export const VideoCall: React.FC = () => {
                     console.error('La conexión ya tiene una descripción de sesión SDP remota establecida');
                     return;
                 }
-                const offer = await peerConnection.current.createOffer();
-                setIsOffer2(() => offer);
+                const offer: RTCLocalSessionDescriptionInit = await peerConnection.current.createOffer();
+
                 await peerConnection.current.setLocalDescription({ type: 'offer', sdp: offer.sdp });
                 console.log('Enviando offer:', offer);
 
-                // Agregar los tracks de video y audio al peer connection
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-                stream.getTracks().forEach(track => {
-                    peerConnection.current?.addTrack(track, stream);
-                });
+                console.log("HANDLE CALL", peerConnection.current.signalingState);
+                    
+                connection.invoke('Offer', JSON.stringify(offer)).catch((err) => console.error(err));
 
-                connection.invoke('Offer', offer.sdp).catch((err) => console.error(err));
+                const offerAnswer:string = await new Promise((resolve) => {
+                    connection.on('answer', (sdp) => {
+                        resolve(sdp);
+                    });
+                });
+                console.log("HANDLE CALL 2", peerConnection.current.signalingState);
+                // Set the remote description with the answer
+                const answerDescription = JSON.parse(offerAnswer);
+                await peerConnection.current.setRemoteDescription(answerDescription);
+                console.log("HANDLE CALL 3", peerConnection.current.signalingState);
             } catch (error) {
                 console.error('Error creando la oferta:', error);
             }
@@ -194,19 +195,19 @@ export const VideoCall: React.FC = () => {
     };
 
     const handleAnswer = async () => {
-        if (peerConnection.current) {
+        if (peerConnection.current && answer) {
             try {
-                const offerSdp = offer2?.sdp;
-                if (offerSdp && typeof offerSdp === 'string' && offerSdp.startsWith('v=')) {
-                    await peerConnection.current.setRemoteDescription({ type: 'offer', sdp: offerSdp });
-                    const answer = await peerConnection.current.createAnswer();
-                    await peerConnection.current.setLocalDescription({ type: 'answer', sdp: answer.sdp });
-                    console.log('Enviando answer:', answer);
-                } else {
-                    console.error('La descripción de sesión SDP no es válida', offerSdp);
-                }
+                console.log("HANDLE ANSWER 1", peerConnection.current.signalingState);
+                await peerConnection.current.setRemoteDescription(answer);
+                console.log("HANDLE ANSWER 2", peerConnection.current.signalingState);
+                const answerAnswer: RTCLocalSessionDescriptionInit = await peerConnection.current.createAnswer()
+                console.log("HANDLE ANSWER 3", peerConnection.current.signalingState);
+                await peerConnection.current.setLocalDescription(answerAnswer);
+                console.log("HANDLE ANSWER 4", peerConnection.current.signalingState);
+                
+                connection.invoke('Answer', JSON.stringify(answerAnswer)).catch((err) => console.error(err));
             } catch (error) {
-                console.error('Error creando la respuesta:', error);
+                console.error('Error enviando la respuesta:', error);
             }
         }
     };
@@ -249,8 +250,8 @@ export const VideoCall: React.FC = () => {
                         gridTemplateColumns: "repeat(4, 1fr)",
                         gap: "0.5rem"
                     }}>
-                        <Button variant="contained" onClick={handleCall}>invitar</Button>
-                        <Button variant="contained" onClick={handleAnswer}>invitar</Button>
+                        <Button variant="contained" onClick={handleCall}>enviar</Button>
+                        <Button variant="contained" onClick={handleAnswer}>recibir</Button>
                         <Button variant="contained" onClick={handleMicState}>{isMicOn ? < MicIcon /> : <MicOffIcon />}</Button>
                         <Button variant="contained" onClick={handleVideoState}>{isVideoOn ? < VideocamIcon /> : <VideocamOffIcon />}</Button>
                         <Button variant="contained">salir</Button>
@@ -268,7 +269,7 @@ export const VideoCall: React.FC = () => {
 
                 }}>
                     {isVideoOn ?
-                        < video id='remoto' style={{ borderRadius: "1rem", width: "100%", height: "100%",  zIndex: "2" }} autoPlay ref={remoteVideoRef} onPlay={() => console.log('Se reprodujo el video remoto')} />
+                        <video id='remoto' style={{ borderRadius: "1rem", width: "100%", height: "100%", zIndex: "2" }} autoPlay ref={remoteVideoRef} onPlay={() => console.log('Se reprodujo el video remoto')} />
                         :
                         <Box sx={{
                             backgroundColor: "#555",
@@ -291,8 +292,8 @@ export const VideoCall: React.FC = () => {
                         gridTemplateColumns: "repeat(4, 1fr)",
                         gap: "0.5rem"
                     }}>
-                        <Button variant="contained" onClick={handleCall}>invitar</Button>
-                        <Button variant="contained" onClick={handleAnswer}>invitar</Button>
+                        <Button variant="contained" onClick={handleCall}>enviar</Button>
+                        <Button variant="contained" onClick={handleAnswer}>recibir</Button>
                         <Button variant="contained" onClick={handleMicState}>{isMicOn ? < MicIcon /> : <MicOffIcon />}</Button>
                         <Button variant="contained" onClick={handleVideoState}>{isVideoOn ? < VideocamIcon /> : <VideocamOffIcon />}</Button>
                         <Button variant="contained">salir</Button>
@@ -302,3 +303,27 @@ export const VideoCall: React.FC = () => {
         </>
     );
 }
+
+
+/*peerConnection.current.onnegotiationneeded = async () => {
+           try {
+               console.log('Negotiation needed');
+               const offer = await peerConnection.current?.createOffer();
+               await peerConnection.current?.setLocalDescription(new RTCSessionDescription({ type: 'offer', sdp: offer?.sdp }));
+               // Send the offer to the remote peer
+               connection.invoke('Offer', offer?.sdp).catch((err) => console.error(err));
+           } catch (error) {
+               console.error('Error creating offer:', error);
+           }
+       };*/
+
+/* connection.on('answer', (sdp) => {
+       try {
+           
+           if (peerConnection.current) {
+               peerConnection.current.setRemoteDescription({ type: 'answer', sdp });
+           }
+       } catch (error) {
+           console.error('Error al actualizar la descripción de sesión SDP:', error);
+       }
+   });*/
